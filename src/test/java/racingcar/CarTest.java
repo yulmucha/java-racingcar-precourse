3494c13bc5bedb9e956bedee1a13564d9a566e00
Yul Jeon <notkhaki@gmail.com>
2022-04-24 15:57:06 +0900

    test: add test for Car

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+
+    @Test
+    void 전진() {
+        Car car = new Car(new CarName("yul"));
+        assertThat(car.getDistance()).isEqualTo(0);
+        car.drive(3);
+        assertThat(car.getDistance()).isEqualTo(0);
+        car.drive(0);
+        assertThat(car.getDistance()).isEqualTo(0);
+        car.drive(4);
+        assertThat(car.getDistance()).isEqualTo(1);
+        car.drive(9);
+        assertThat(car.getDistance()).isEqualTo(2);
+    }
+}
