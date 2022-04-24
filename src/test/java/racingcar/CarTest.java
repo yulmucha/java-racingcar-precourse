03977fb410adbc91866220f934d1e0d4c1597a29
Yul Jeon <notkhaki@gmail.com>
2022-04-24 16:16:01 +0900

    test: add test for Cars

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -7,15 +7,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CarTest {
 
     @Test
-    void 전진() {
+    void 전진_정지() {
         Car car = new Car(new CarName("yul"));
         assertThat(car.getDistance()).isEqualTo(0);
+
         car.drive(3);
         assertThat(car.getDistance()).isEqualTo(0);
+
         car.drive(0);
         assertThat(car.getDistance()).isEqualTo(0);
+
         car.drive(4);
         assertThat(car.getDistance()).isEqualTo(1);
+
         car.drive(9);
         assertThat(car.getDistance()).isEqualTo(2);
     }
---
+++ src/test/java/racingcar/CarsTest.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarsTest {
+
+    @Test
+    void 전진_정지() {
+        List<Car> carList = Arrays.asList(
+                new Car(new CarName("yul")),
+                new Car(new CarName("dd")),
+                new Car(new CarName("iu")));
+        Cars cars = new Cars(carList);
+
+        cars.driveAll(Arrays.asList(2,4,3));
+
+        assertThat(cars.getList().get(0).getDistance()).isEqualTo(0);
+        assertThat(cars.getList().get(1).getDistance()).isEqualTo(1);
+        assertThat(cars.getList().get(2).getDistance()).isEqualTo(0);
+    }
+}
